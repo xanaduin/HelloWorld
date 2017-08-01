d0fb965845d7ad93fd170a9e172c1a01ee942705
aksdi <aksdi@microsoft.com>
2017-08-01 23:27:29 +0530

    Hello Class file

---
+++ Hello.cs
@@ -0,0 +1,27 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Hello.cs" company="Akshay Dixit (xanaduin)">
+//   Copyright ©  2017
+// </copyright>
+// <summary>
+//   Displays hello world on console.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console Writeline to print hello world
+            Console.WriteLine("Hello, world!");
+
+            //Console Readline to make the prompt stay post display of line
+            Console.ReadLine();
+        }
+    }
+}
